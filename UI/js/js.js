b90180d8a09e8d983b81dfdcdb1658044e36c519
Norrissim <zorinhand@mail.ru>
2016-03-20 11:15:59 +0300

    Added localstorage dependence. Working adding messages and changing username

--- UI/js/js.js
+++ UI/js/js.js
@@ -1,4 +1,7 @@
-var gloablTempSt;
+var globalTempSt;
+var globalCurrentUsername;
+var messageList = [];
+
 function run() {
     var usernameButton = document.getElementsByClassName('changeUsername')[0];
     var messageButton = document.getElementsByClassName('enterMessage')[0];
@@ -7,12 +10,140 @@ function run() {
     messageButton.addEventListener('click', onMessageEnter);
 
     var centerPart = document.getElementsByClassName('centralPart')[0];
-    centerPart.addEventListener('click', onMassegeClick);
+    centerPart.addEventListener('click', onMassageClick);
+
+    initMessageList();
+
+    globalCurrentUsername = loadUsernameFromLocalStorage();
+    initUsername();
+
+    render(messageList);
 
     centerPart.scrollTop = centerPart.scrollHeight;
 }
 
-function onMassegeClick(evtObj) {
+function render(messages) {
+    var items = document.getElementsByClassName('centralPart')[0];
+    var children = items.children;
+    while(children.length > 0) {
+        items.removeChild(children[0]);
+    }
+    for(var i = 0; i < messages.length; i++) {
+        if(messages[i].author == globalCurrentUsername) {
+            renderAllyMessage(messages[i]);
+        }
+        else {
+            renderEnemyMessage(messages[i]);
+        }
+    }
+}
+
+function initMessageList() {
+    messageList = loadMessages();
+    if(messageList == null) {
+        messageList = [];
+        saveMessages(messageList);
+        messageList = loadMessages();
+    }
+}
+
+function renderAllyMessage(messaage){
+    var items = document.getElementsByClassName('centralPart')[0];
+    var element = elementFromTemplate();
+    element.classList.add('messageAlly');
+    renderMessageValues(element, messaage);
+
+    items.appendChild(element);
+}
+
+function renderEnemyMessage(messaage){
+    var items = document.getElementsByClassName('centralPart')[0];
+    var element = elementFromTemplate();
+    element.classList.add('messageEnemy');
+    renderMessageValues(element, messaage);
+
+    items.appendChild(element);
+}
+
+function renderMessageValues(element, message) {
+    element.setAttribute('data-mes-id', message.id);
+    var d = new Date(message.time);
+    var validDate = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+    element.firstElementChild.nextElementSibling.nextElementSibling.textContent = validDate;
+    element.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.textContent = message.author + " :";
+    element.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent = message.message;
+}
+
+function elementFromTemplate() {
+    var template = document.getElementById("message-template");
+
+    return template.firstElementChild.cloneNode(true);
+}
+
+function saveUsernameInLocalStorage() {
+    if(typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+
+    localStorage.setItem("CurrentUsername", JSON.stringify(globalCurrentUsername));
+}
+
+function loadUsernameFromLocalStorage() {
+    if(typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+
+    var item = localStorage.getItem("CurrentUsername");
+
+    return item && JSON.parse(item);
+}
+
+function saveMessages(listToSave) {
+    if(typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+
+    localStorage.setItem("MessagesList", JSON.stringify(listToSave));
+}
+
+function loadMessages() {
+    if(typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+
+    var item = localStorage.getItem("MessagesList");
+
+    if(item == 'undefined')
+    {
+        return null;
+    }
+
+    return item && JSON.parse(item);
+}
+
+function uniqueId() {
+    var date = Date.now();
+    var random = Math.random() * Math.random();
+
+    return Math.floor(date * random);
+}
+
+function newMessage(text, author) {
+    var d = new Date();
+    var t = d.getTime();
+    return {
+        message:text,
+        author: author,
+        time: d,
+        id: '' + uniqueId()
+    };
+}
+
+function onMassageClick(evtObj) {
     if (evtObj.type === 'click' && evtObj.target.classList.contains('utilDelete')) {
         onDeleteButtonClick(evtObj);
     }
@@ -29,14 +160,17 @@ function onDontChangeButtonClick(evtObj) {
     var inputForNewMessage = divForMessage.lastElementChild.previousSibling;
     var buttonDontSave = divForMessage.lastElementChild;
     var divForText = document.createElement('div');
-    divForText.innerText = gloablTempSt;
+    divForText.innerText = globalTempSt;
     divForMessage.removeChild(buttonDontSave);
     divForMessage.removeChild(inputForNewMessage);
     divForMessage.appendChild(divForText);
 }
 
 function onDeleteButtonClick(evtObj) {
-    evtObj.target.parentElement.remove();
+    var index = indexByElement(evtObj.target, messageList);
+    messageList.splice(index, 1);
+    render(messageList);
+    saveMessages(messageList);
 }
 
 function onChangeButtonClick(evtObj) {
@@ -49,7 +183,7 @@ function onChangeButtonClick(evtObj) {
         buttonDontSave.appendChild(document.createTextNode("Don't save"));
         var divForText = divForMessage.lastElementChild;
         var tempMes = divForText.innerText;
-        gloablTempSt = divForText.innerText;
+        globalTempSt = divForText.innerText;
         var inputForNewMessage = document.createElement('input');
         inputForNewMessage.classList.add('editname');
         inputForNewMessage.value = tempMes;
@@ -81,8 +215,16 @@ function onUsernameChange() {
     }
     else {
         changeButton.innerText = "Ch-ch-change";
+        globalCurrentUsername = editChangeName.value;
         editChangeName.disabled = true;
+        saveUsernameInLocalStorage();
+        render(messageList);
     }
+}
+
+function initUsername() {
+    var editChangeName = document.getElementsByClassName("editname")[0];
+    editChangeName.value = globalCurrentUsername;
 }
 
 function onMessageEnter() {
@@ -90,19 +232,29 @@ function onMessageEnter() {
     var authorName = document.getElementsByClassName('editname')[0];
     var centerPart = document.getElementsByClassName('centralPart')[0];
 
-    addMessage(newMessage.value, authorName.value);
+    var mes = addMessage(newMessage.value);
     newMessage.value = '';
+    render(messageList);
     centerPart.scrollTop = centerPart.scrollHeight;
 }
 
-function addMessage(value, author) {
-    if (!value || !author) {
+function addMessage(value) {
+    if (!value || !loadUsernameFromLocalStorage()) {
         return;
     }
-    var item = createItem(value, author);
-    var items = document.getElementsByClassName('centralPart')[0];
+    var newMes = newMessage(value, loadUsernameFromLocalStorage());
 
-    items.appendChild(item);
+    messageList.push(newMes);
+    saveMessages(messageList);
+    return newMes;
+}
+
+function indexByElement(element, messages){
+    var id = element.parentElement.getAttribute('data-mes-id');
+
+    return messages.findIndex(function(item) {
+        return item.id == id;
+    });
 }
 
 function createItem(text, author) {
